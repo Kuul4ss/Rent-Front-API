325498d043df9b7b198d06b0bcef22ad41e25f74
lucas <lucas.953501@gmail.com>
2023-06-03 17:42:34 +0200

    init server

---
+++ src/main/java/com/example/rentfrontapi/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.rentfrontapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
